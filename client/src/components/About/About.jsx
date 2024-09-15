b2d7093f6028930bb3518d9280dd9a77584ce529
mohitKundu01 <144689971+mohitKundu01@users.noreply.github.com>
2024-09-16 02:34:27 +0530

    Update About.jsx

--- client/src/components/About/About.jsx
+++ client/src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import styles from './about.module.css';
+import styles from './About.module.css';
 import vector from "../../assets/Vector.png"
 const About = () => {
     return (
